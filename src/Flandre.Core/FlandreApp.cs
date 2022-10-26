8a8bb732cbbcf596115cfc883419550c88b190c7
b1acksoil <dev@sorabs.cc>
2022-10-27 03:41:10 +0800

    style(core): split app and config in separate files

--- src/Flandre.Core/FlandreApp.cs
+++ src/Flandre.Core/FlandreApp.cs
@@ -296,21 +296,3 @@ public class FlandreApp
         ctx.Bot.SendMessage(ctx.Message, $"未找到指令：{root}。").Wait();
     }
 }
-
-/// <summary>
-/// 应用配置
-/// </summary>
-public class FlandreAppConfig
-{
-    /// <summary>
-    /// 全局指令前缀
-    /// </summary>
-    public string CommandPrefix { get; set; } = "";
-
-    /// <summary>
-    /// 忽略未定义指令的调用。可用值为：
-    /// <br/> no - （默认）不忽略，调用未定义指令时发出警告信息
-    /// <br/> root - 忽略根指令（顶级指令）
-    /// </summary>
-    public string IgnoreUndefinedCommand { get; set; } = "no";
-}
---
+++ src/Flandre.Core/FlandreAppConfig.cs
@@ -0,0 +1,19 @@
+﻿namespace Flandre.Core;
+
+/// <summary>
+/// 应用配置
+/// </summary>
+public class FlandreAppConfig
+{
+    /// <summary>
+    /// 全局指令前缀
+    /// </summary>
+    public string CommandPrefix { get; set; } = "";
+
+    /// <summary>
+    /// 忽略未定义指令的调用。可用值为：
+    /// <br/> no - （默认）不忽略，调用未定义指令时发出警告信息
+    /// <br/> root - 忽略根指令（顶级指令）
+    /// </summary>
+    public string IgnoreUndefinedCommand { get; set; } = "no";
+}
