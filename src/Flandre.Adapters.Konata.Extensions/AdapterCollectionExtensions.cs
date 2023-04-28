6bd6b9e0eccf8ca5486108ef52de695d92168875
bsdayo <dev@sorabs.cc>
2023-04-29 05:54:08 +0800

    feat: add adapter extensions

---
+++ src/Flandre.Adapters.Konata.Extensions/AdapterCollectionExtensions.cs
@@ -0,0 +1,51 @@
+﻿using Flandre.Framework;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Flandre.Adapters.Konata.Extensions;
+
+/// <summary>
+/// Konata 适配器扩展
+/// </summary>
+public static class AdapterCollectionExtensions
+{
+    /// <summary>
+    /// 添加 Konata 适配器，自动从配置根中的 <c>Adapters:Konata</c> 项读取配置。
+    /// </summary>
+    public static void AddKonata(this IAdapterCollection adapters)
+    {
+        var config = adapters.Services
+            .BuildServiceProvider()
+            .GetRequiredService<IConfigurationRoot>()
+            .GetSection("Adapters:Konata")
+            .Get<KonataAdapterConfig>();
+        adapters.Add(new KonataAdapter(config ?? new KonataAdapterConfig()));
+    }
+
+    /// <summary>
+    /// 添加 Konata 适配器。
+    /// </summary>
+    public static void AddKonata(this IAdapterCollection adapters, IConfiguration configuration)
+    {
+        var config = configuration.Get<KonataAdapterConfig>();
+        adapters.Add(new KonataAdapter(config ?? new KonataAdapterConfig()));
+    }
+
+    /// <summary>
+    /// 添加 Konata 适配器。
+    /// </summary>
+    public static void AddKonata(this IAdapterCollection adapters, Action<KonataAdapterConfig> action)
+    {
+        var config = new KonataAdapterConfig();
+        action(config);
+        adapters.Add(new KonataAdapter(config));
+    }
+
+    /// <summary>
+    /// 添加 Konata 适配器。
+    /// </summary>
+    public static void AddKonata(this IAdapterCollection adapters, KonataAdapterConfig config)
+    {
+        adapters.Add(new KonataAdapter(config));
+    }
+}
---
+++ src/Flandre.Adapters.OneBot.Extensions/AdapterCollectionExtensions.cs
@@ -0,0 +1,52 @@
+﻿using Flandre.Framework;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Flandre.Adapters.OneBot.Extensions;
+
+/// <summary>
+/// OneBot 适配器扩展
+/// </summary>
+public static class AdapterCollectionExtensions
+{
+    /// <summary>
+    /// 添加 OneBot 适配器，自动从配置根中的 <c>Adapters:OneBot</c> 项读取配置。
+    /// </summary>
+    public static void AddOneBot(this IAdapterCollection adapters)
+    {
+        var config = adapters.Services
+            .BuildServiceProvider()
+            .GetRequiredService<IConfigurationRoot>()
+            .GetSection("Adapters:OneBot")
+            .Get<OneBotAdapterConfig>();
+        adapters.Add(new OneBotAdapter(config ?? new OneBotAdapterConfig()));
+    }
+
+    /// <summary>
+    /// 添加 OneBot 适配器。
+    /// </summary>
+    public static void AddOneBot(this IAdapterCollection adapters, IConfiguration configuration)
+    {
+        var config = configuration.Get<OneBotAdapterConfig>();
+        adapters.Add(new OneBotAdapter(config ?? new OneBotAdapterConfig()));
+    }
+
+    /// <summary>
+    /// 添加 OneBot 适配器。
+    /// </summary>
+    public static void AddOneBot(this IAdapterCollection adapters, Action<OneBotAdapterConfig> action)
+    {
+        var config = new OneBotAdapterConfig();
+        action(config);
+        adapters.Add(new OneBotAdapter(config));
+    }
+
+    /// <summary>
+    /// 添加 OneBot 适配器。
+    /// </summary>
+    public static void AddOneBot(this IAdapterCollection adapters, OneBotAdapterConfig config)
+    {
+        adapters.Add(new OneBotAdapter(config));
+    }
+}
+
